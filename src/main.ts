4d90c8c4ee60502386f151d7a016c46acd26e9e6
deivisonisidoro <deivisonisidoro@gmail.com>
2022-02-25 09:40:06 -0300

    PORT

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { env } from 'process';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3333);
+  await app.listen(env.PORT || 3333);
 }
 bootstrap();
